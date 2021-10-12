8677d18d4bdb955174835efad8422afca6e07456
Rerer-er <19it2.pralich.a@pdu.by>
2021-10-12 17:15:37 +0300

    Added Entities(class library), Profile model and profilecontext

---
+++ ProfileMicroservice/ProfileMicroservice/Context/ProfileContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProfileMicroservice.Context
+{
+    public class ProfileContext : DbContext
+    {
+        public ProfileContext(DbContextOptions options) : base(options)
+        {
+
+        }
+    }
+}
