4b9f80b7de9d0578ff45266868fda0b445bec2fe
Rerer-er <19it2.pralich.a@pdu.by>
2021-10-18 21:34:10 +0300

    changed mediator(getallprfile)

--- ProfileMicroservice/Repository/BaseRepository/BaseRepository.cs
+++ ProfileMicroservice/Repository/BaseRepository/BaseRepository.cs
@@ -17,8 +17,21 @@ namespace Repository
             _context.Set<T>().AsNoTracking() : _context.Set<T>();
         public IQueryable<T> ReturnDistinct(Expression<Func<T, bool>> expression, bool trackChanges) => !trackChanges ?
             _context.Set<T>().Where(expression).AsNoTracking() : _context.Set<T>().Where(expression);
-        public void Create(T entity) => _context.Set<T>().Add(entity);
-        public void Update(T entity) => _context.Set<T>().Update(entity);
-        public void Delete(T entity) => _context.Set<T>().Remove(entity);
+        public void Create(T entity) 
+        { 
+            _context.Set<T>().Add(entity);
+            _context.SaveChanges();
+        }
+        public void Update(T entity)
+        {
+            _context.Set<T>().Update(entity);
+            _context.SaveChanges();
+        }
+        public void Delete(T entity)
+        {
+            _context.Set<T>().Remove(entity);
+            _context.SaveChanges();
+        }
+            
     }
 }
